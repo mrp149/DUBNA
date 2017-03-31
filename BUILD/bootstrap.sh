c3613748745e20a40fda18ed97f81487d1c754f2
mrp149 <mikhailp@pacbell.net>
2017-03-30 23:01:06 -0700

    almost-phase1

--- BUILD/bootstrap.sh
+++ BUILD/bootstrap.sh
@@ -15,29 +15,41 @@ echo Populating disks in $HOME/.besm6
 
 ./populate-dsk.sh
 
-echo Making disk images for SIMH
-( cd data; ./mk-disk9.sh; ./mk-disk11.sh)
 
 echo Build DD
-
-mk-ld-src.sh
+rm -f $HOME/.besm6/1999
+./mk-dd-src.sh
 
 #echo Build MS
-
+#rm -f $HOME/.besm6/1999
 #./mk-ms-src.sh
 
 echo Build LD
-
+rm -f $HOME/.besm6/1999
 ./mk-ld-src.sh
 
-echo Making the patch deck and link DD
-./mk-deck.sh
+
+echo Making disk images for SIMH
+( cd data; ./mkdisk9.sh; ./mkdisk11.sh)
+
+echo Making the patch deck
+touch $HOME/.besm6/1999
+
+( cd deck-b1; ./mk-deck-b1.sh) > deck-b1.b6 
+echo compiling ... and linking ....
+dispak -l deck-b1.b6 | tee /dev/tty > deck-b1.list
+
 
 echo Run DEBMULT
-./mk-debmult.sh
+touch $HOME/.besm6/1999
+dispak -l debmult.b6 | tee /dev/tty > debmult.list
 
-./besm6 -i first-ddboot.ini
+echo Making drum images for SIMH
+( cd data; ./mkdrumdd.sh)
 
-echo The system is ready 
-echo use besm6 -i ddubna.ini
-echo Consul port 4199
+echo All done and ready for the first boot!
+sleep 3
+
+./besm6 -i first-dd-boot.ini
+
+cat ReadMeBoot.txt
